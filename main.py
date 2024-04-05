6477124ebd71442cfacc149a7b17921456aad16b
saira2911 <anasaira29@gmail.com>
2024-04-05 11:24:16 +0200

    add load and exploration

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+#%%
 import pandas as pd
-from src import soporte_evaluacion as ev
+from src import support_transformation as sp
+#%%
+df_flight_activity = pd.read_csv("files/Customer Flight Activity.csv")
+df_loyalty_history = pd.read_csv("files/Customer Loyalty History.csv")
+
+df_flight_activity.name = "Flight Activity"
+df_loyalty_history.name = "Loyalty History"
+
+lista_dfs = [df_flight_activity, df_loyalty_history]
+
+# %%
+sp.exploracion_general(lista_dfs)
+sp.exploracion_columna(lista_dfs)
+
+
+# %%
+sp.graficos_boxp(df_flight_activity, ["Flights Booked", "Flights with Companions", "Total Flights"])
+sp.graficos_hisp(df_loyalty_history, ["Gender", "Education", "Marital Status"])
+# %%
---
+++ src/support_transformation.py
@@ -0,0 +1,140 @@
+
+# Importaciones
+# -----------------------------------------------------------------------
+import pandas as pd
+import numpy as np 
+from IPython.display import display
+
+# Imputación de nulos
+# -----------------------------------------------------------------------
+from sklearn.impute import SimpleImputer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.impute import KNNImputer
+
+# Librerías de visualización
+# -----------------------------------------------------------------------
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Configuración
+# -----------------------------------------------------------------------
+pd.set_option('display.max_columns', None) # para poder visualizar todas las columnas de los DataFrames
+
+##FUNCIONES DE EXPLORACION##
+
+def exploracion_general (lista):
+    """Esta función proporciona toda la informacion necesaria de uno o varios DataFrame
+    
+    Args:
+    lista : lista de los DataFrame que queremos explorar
+    
+    Returns:
+    La funcion no tiene return pero devuelve varios prints con
+    la informacion que necesitamos:
+    - Descripciones separadas por columnas numericas y categoricas
+    - Tipos de dato por columna
+    - Numero total de filas y columnas
+    - Informacion adicional
+    - Total de nulos
+    - Total de duplicados"""
+
+
+    for df in lista:
+
+        print("------Exploracion del dataframe: {} ------".format(df.name))
+        try:
+            print("-------Descripción columnas numéricas:---------")
+            print(df.describe())
+        except:
+            print("Este DataFrame no contiene columnas numericas")
+
+        try:
+            print("-------Descripción columnas categoricas:---------")
+            print(df.describe(include="O"))
+        except: 
+            print("Este DataFrame no contiene columnas categoricas")
+
+        print("------Tipos de datos:---------")
+        print(df.dtypes)
+        print("------Numero de filas y columnas:------")
+        print(df.shape)
+        print("------Información adicional:---------")
+        print(df.info())
+        print("------Cantidad de nulos:---------")
+        print(df.isnull().sum())
+        print("------Cantidad de duplicados:---------")
+        print(df.duplicated().sum())
+    
+def exploracion_columna (lista):
+
+    """Esta función proporciona toda la informacion necesaria de cada columna del DataFrame
+    
+    Args:
+    lista : lista de los DataFrame que queremos explorar
+    
+    Returns:
+    La funcion no tiene return pero devuelve varios prints con
+    la informacion que necesitamos:
+    - Frecuencia de cada valor unico de la columna
+    - Total de nulos
+    - Total de duplicados"""
+
+    for dataframe in lista:
+        print("------Exploracion del dataframe: {} ------".format(dataframe.name))
+        for columna in list(dataframe.columns):
+            print(f" \n----------- ESTAMOS ANALIZANDO LA COLUMNA: '{columna.upper()}' -----------\n")
+            print(f"* Frecuencia de valores en la columna: \n {dataframe[columna].value_counts()}")
+            print(f"La suma de datos nulos {dataframe[columna].isnull().sum()}")
+            print(f"La suma de datos duplicados {dataframe[columna].duplicated().sum()}")
+            
+def graficos_hisp (dataframe, lista_col):
+
+    """Esta función crea graficos histplot para las columnas y el dataframe indicados
+    
+    Args:
+    dataframe : DF que contiene las columnas que queremos analizar
+    lista_col : lista de columnas que queremos analizar del DF anterior
+    
+    Returns:
+    La funcion devuelve un grafico histplot por cada columna indicada
+    en la lista"""
+
+    fig, axes = plt.subplots(len(lista_col), 1, figsize=(30,20))
+
+    axes = axes.flat
+
+    for indice, columna in enumerate(lista_col):
+
+        sns.histplot(x = columna, data = dataframe, color = "purple", kde = True, bins = 15, ax=axes[indice])
+        axes[indice].set_title(columna)
+        axes[indice].set_xlabel("Frecuencia")
+        plt.gca().spines[["top", "right"]].set_visible(False)
+
+        fig.tight_layout();
+
+def graficos_boxp (dataframe, lista_col):
+
+    """Esta función crea graficos boxplot para las columnas y el dataframe indicados
+    
+    Args:
+    dataframe : DF que contiene las columnas que queremos analizar
+    lista_col : lista de columnas que queremos analizar del DF anterior
+    
+    Returns:
+    La funcion devuelve un grafico boxplot por cada columna indicada
+    en la lista"""
+
+    fig, axes = plt.subplots(len(lista_col), 1, figsize=(30,20))
+
+    axes = axes.flat
+
+    for indice, columna in enumerate(lista_col):
+
+        sns.boxplot(y = columna, data = dataframe, color = "purple", ax=axes[indice])
+        axes[indice].set_title(columna)
+        axes[indice].set_ylabel(columna)
+
+        fig.tight_layout();
+
+
